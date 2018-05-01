feae7d846a5deebc2fba7b605264094d0debe7de
kissge <mail@yo.eki.do>
2018-05-01 22:44:46 +0900

    Change IME shortcut (Win + SPC not always working)

--- keyboards/helix/rev2/keymaps/default/keymap.c
+++ keyboards/helix/rev2/keymaps/default/keymap.c
@@ -132,7 +132,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case IME:
       if (record->event.pressed) {
-        SEND_STRING(SS_LGUI(" "));
+        SEND_STRING(SS_LALT("`"));
       }
       return false;
     case NEXT:
